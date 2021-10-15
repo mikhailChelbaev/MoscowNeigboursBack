36a140dbc9bd63bcccc2c9fa3537799a424a1a4f
Mikhail <chelbaev.mihail70@gmail.com>
2021-10-16 00:41:08 +0300

    create jar

---
+++ src/main/java/com/moscow/neighbours/backend/config/BaseConfiguration.java
@@ -0,0 +1,11 @@
+package com.moscow.neighbours.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Additional bean configuration
+ */
+@Configuration
+public class  BaseConfiguration {
+}
---
+++ src/main/java/com/moscow/neighbours/backend/config/CronConfig.java
@@ -0,0 +1,27 @@
+package com.moscow.neighbours.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/**
+ * Configuration for scheduling tasks using CRON
+ */
+@Configuration
+@EnableScheduling
+public class CronConfig implements SchedulingConfigurer {
+    private static final int POOL_SIZE = 1;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("my-scheduled-task-pool-");
+        threadPoolTaskScheduler.initialize();
+
+        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
