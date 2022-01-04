7f651857d74e8db8107cfe481d56c503fca96f5e
Mikhail <chelbaev.mihail70@gmail.com>
2022-01-04 17:24:17 +0300

    authorization + user controller

--- src/main/java/com/moscow/neighbours/backend/config/BaseConfiguration.java
+++ src/main/java/com/moscow/neighbours/backend/config/BaseConfiguration.java
@@ -1,5 +1,7 @@
 package com.moscow.neighbours.backend.config;
 
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,8 +12,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class  BaseConfiguration {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
     @Bean("encoder")
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean("algorithm")
+    Algorithm algorithm() {
+        return Algorithm.HMAC256(jwtSecret.getBytes());
     }
 }
--- src/main/java/com/moscow/neighbours/backend/config/WebSecurityConfig.java
+++ src/main/java/com/moscow/neighbours/backend/config/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.moscow.neighbours.backend.config;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import com.moscow.neighbours.backend.filters.CustomAuthenticationFilter;
 import com.moscow.neighbours.backend.filters.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,24 +25,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // MARK: - property
 
-//    private final UserDetailsService userDetailsService;
-//
-//    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private Algorithm algorithm;
 
     // MARK: - methods
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-//    }
+    @Autowired
+    void getAlgorithm(ApplicationContext context) {
+        algorithm = context.getBean("algorithm", Algorithm.class);
+    }
+
+    @Bean
+    CustomAuthorizationFilter authorizationFilter() {
+        return new CustomAuthorizationFilter(algorithm);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests().anyRequest().permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
 //        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(authorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 //    @Bean
